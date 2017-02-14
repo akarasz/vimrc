f02dfa6cca6120b99af67d1f4417af78b30924fe
András Kárász <karaszandris@gmail.com>
2017-02-14 15:35:53 +0100

    added youCompleteMe compile code to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# change working directory to vim home
-cd $(dirname $(realpath install.sh))
+BASEDIR=$(dirname $(realpath install.sh))
 
-# initialize submodules
+# change working directory to vim home
+echo "initialize bundles..."
+
+cd ${BASEDIR}
 git submodule update --init --recursive
+
+echo "done."
+
+# YouCompleteMe
+echo "compile YouCompleteMe"
+echo "(NEEDS CMAKE, G++ AND PYTHON-DEVEL PACKAGES)"
+
+YCM_BUILD=${BASEDIR}/build/ycm_build
+
+mkdir -p ${YCM_BUILD}
+cd ${YCM_BUILD}
+
+cmake -G "Unix Makefiles" . ${BASEDIR}/bundle/YouCompleteMe/third_party/ycmd/cpp
+cmake --build . --target ycm_core
